1295032337a959bc6b3f40bdf112e13d952e86db
Felix Ostrowski <felix.ostrowski@googlemail.com>
2017-08-21 17:16:52 +0200

    Remove superfluous demo emit

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -11,12 +11,7 @@ import translate from './translate'
 const ItemList = ({ translate, listItems, emitter }) => (
   <ul className="ItemList" >
     {listItems.map(listItem => (
-      <li
-        key={listItem.about['@id']}
-        onMouseEnter={() => {
-          emitter.emit('hoverListItem',  { id: listItem.about["@id"] })
-        }}
-      >
+      <li key={listItem.about['@id']}>
         <Icon type={listItem.about['@type']} />
         <Link to={listItem.about['@id']}>
           {translate(listItem.about.name) || listItem.about['@id']}
