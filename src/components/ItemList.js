d26f957045d40e44ce77c716a49df2bba974a33a
Felix Ostrowski <felix.ostrowski@googlemail.com>
2017-11-09 12:22:28 +0100

    Show hierarchy for Offers

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -8,17 +8,30 @@ import '../styles/ItemList.pcss'
 
 import translate from './translate'
 
+const top = item => item.about['@type'] !== 'Offer' || item.about.superOffer['@type'] === 'Product'
+
 const ItemList = ({ translate, listItems }) => (
   <ul className="ItemList" >
-    {listItems.map(listItem => (
+    {listItems.filter(top).map(listItem => (
       <li key={listItem.about['@id']}>
         <Link to={listItem.about['@id']}>
           <Icon type={listItem.about['@type']} />&nbsp;
           <Label item={listItem.about} />
         </Link>
+        {listItem.about.subOffer &&
+          <ul>
+            {listItem.about.subOffer.map(subOffer =>
+              <li key={subOffer['@id']}>
+                <Link to={subOffer['@id']}>
+                  <Icon type={subOffer['@type']} />&nbsp;
+                  <Label item={subOffer} />
+                </Link>
+              </li>
+            )}
+          </ul>
+        }
       </li>
-    )
-    )}
+    ))}
   </ul>
 )
 
